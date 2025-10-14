bc8ea0fb77b2b6d630f0a66fb72b2821f98483bf
samuelleonard63 <samuelleonard63@gmail.com>
2025-10-14 10:08:44 +0000

    installed spactie activityogs and updated department migration

--- database/migrations/2025_10_14_094236_create_departments_table.php
+++ database/migrations/2025_10_14_094236_create_departments_table.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+            $table->string('code', 10)->unique()->nullable();
+            $table->foreignId('head_user_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('deputy_head_user_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('departments');
